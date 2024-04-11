f467cb9eaa43cc68ece76f06028e306fc8aecc44
novelkhan <novel4004@gmail.com>
2024-04-12 05:49:49 +0600

    Add project files.

---
+++ MongoDbCrud2AspNetCoreMVC/Models/Person.cs
@@ -0,0 +1,21 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
+
+namespace MongoDbCrud2AspNetCoreMVC.Models
+{
+    public class Person
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string _id { get; set; }
+
+        [BsonElement("PersonName")]
+        public string PersonName { get; set; }
+
+        [BsonElement("PersonAge")]
+        public int PersonAge { get; set; }
+
+        [BsonElement("HomeTown")]
+        public string HomeTown { get; set; }
+    }
+}
---
+++ MongoDbCrud2AspNetCoreMVC/Services/MongoDbService.cs
@@ -0,0 +1,42 @@
+﻿using MongoDB.Driver;
+using MongoDbCrud2AspNetCoreMVC.Models;
+
+namespace MongoDbCrud2AspNetCoreMVC.Services
+{
+    public class MongoDbService
+    {
+        private readonly IMongoCollection<Person> _collection;
+
+        public MongoDbService()
+        {
+            var client = new MongoClient("mongodb+srv://nvl:q3mfFJz5HM68eLNk@cluster1.2auwomk.mongodb.net/PersonDB2");
+            var database = client.GetDatabase("PersonDB2");
+            _collection = database.GetCollection<Person>("Persons");
+        }
+
+        public async Task<List<Person>> GetAll()
+        {
+            return await _collection.Find(_ => true).ToListAsync();
+        }
+
+        public async Task<Person> GetById(string id)
+        {
+            return await _collection.Find(person => person._id == id).FirstOrDefaultAsync();
+        }
+
+        public async Task Create(Person person)
+        {
+            await _collection.InsertOneAsync(person);
+        }
+
+        public async Task Update(string id, Person person)
+        {
+            await _collection.ReplaceOneAsync(p => p._id == id, person);
+        }
+
+        public async Task Delete(string id)
+        {
+            await _collection.DeleteOneAsync(p => p._id == id);
+        }
+    }
+}
